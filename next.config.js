e463b34831b5d41e6b6df45bab4002593d256717
= <=>
2023-01-04 11:48:53 +0530

    changed import

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     SERVER: process.env.SERVER,
+    MYSQL_HOST: process.env.MYSQL_HOST,
+    MYSQL_USERNAME: process.env.MYSQL_USERNAME,
+    MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+    MYSQL_DB: process.env.MYSQL_DB,
   },
 }
 
