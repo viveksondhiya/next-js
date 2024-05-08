dbc5c94c69f748c85aa2274a5f3a46535954477a
Vivek Sondhiya <viveksondhiya555@gmail.com>
2024-05-09 00:57:34 +0530

    first

--- helpers/Paginate.js
+++ helpers/Paginate.js
@@ -1,4 +1,7 @@
 export const Paginate = (data, pageNumber , pageSize) => {
+    if(data.length===0){
+        return [];
+    }
     const startIndex = (pageNumber - 1)*pageSize ; 
     return data.slice(startIndex , startIndex + pageSize);
 }
